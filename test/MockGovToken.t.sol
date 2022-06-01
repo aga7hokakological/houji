c63d952508bb9be462856d904ebff5c2dbb85ab1
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2022-06-01 21:03:11 +0530

    done

---
+++ test/MockGovToken.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.12;
+
+import "forge-std/Test.sol";
+// import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+import "lib/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+
+contract MockGovTokenTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
